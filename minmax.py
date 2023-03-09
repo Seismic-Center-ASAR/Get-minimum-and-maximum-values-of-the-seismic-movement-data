35d4086b7d3da211b9860ad81d67e54e2caef5d3
Seismic-Center-ASAR <127415190+Seismic-Center-ASAR@users.noreply.github.com>
2023-03-09 17:25:49 +0200

    Add files via upload

---
+++ minmax.py
@@ -0,0 +1,21 @@
+from obspy import UTCDateTime
+from obspy.clients.seedlink import Client
+
+client = Client("geofon.gfz-potsdam.de", 18000)
+buffer_size = 60  # in seconds
+endtime = UTCDateTime.now()
+starttime = endtime - buffer_size
+
+# Continuously get and print the waveform data
+while True:
+    st = client.get_waveforms("RO", "VRI", "", "EHZ", starttime, endtime)
+    trace = st[0]
+    data = trace.data
+    print("Minimum value: ", min(data), "Maximum value: ", max(data))
+    min_index = data.argmin()
+    max_index = data.argmax()
+    print("Data with minimum value: ", data[min_index], "Data with maximum value: ", data[max_index])
+    
+    # Update the start and end times for the next iteration
+    endtime = UTCDateTime.now()
+    starttime = endtime - buffer_size
